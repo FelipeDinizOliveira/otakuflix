f92377bd9733a60bc976a0a8a30e663d6bfa3bb4
Felipe Diniz <lipedniz47@gmail.com>
2025-09-25 14:08:58 -0300

    vercelles para api do manga funcionar 2.0

--- API/mangas.js
+++ API/mangas.js
@@ -1,18 +1,17 @@
-import fetch from "node-fetch";
+import axios from "axios";
 
 export default async function handler(req, res) {
+  const { title = "" } = req.query;
+
   try {
-    const { title = "" } = req.query;
+    const response = await axios.get(`https://api.mangadex.org/manga`, {
+      params: { title, limit: 4, includes: ["cover_art"] },
+    });
 
-    const url = `https://api.mangadex.org/manga?title=${encodeURIComponent(
-      title
-    )}&limit=4&includes[]=cover_art`;
-    const response = await fetch(url);
-    const data = await response.json();
-
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).json(data);
+    // retorna sempre data
+    res.status(200).json({ data: response.data.data || [] });
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    console.error(err);
+    res.status(500).json({ data: [] });
   }
 }
